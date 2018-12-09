38bd76c0629e0e06953ec701438b2e9ee71c06c9
Stephen Leyva <sleyva@staq.com>
2018-12-09 09:58:48 -0500

    Use GCS as terrafrom state store

--- provider.tf
+++ provider.tf
@@ -1,7 +1,18 @@
 variable GOOGLE_PROJECT_ID {}
 variable GOOGLE_COMPUTE_ZONE {}
+variable TERRAFORM_STATE_BUCKET {}
+variable ENVIRONMENT {}
 
 provider "google" {
   project = "${var.GOOGLE_PROJECT_ID}"
   zone    = "${var.GOOGLE_COMPUTE_ZONE}"
+}
+
+data "terraform_remote_state" "bucket_state" {
+  backend = "gcs"
+
+  config {
+    bucket = "${var.TERRAFORM_STATE_BUCKET}"
+    prefix = "${var.ENVIRONMENT}"
+  }
 }
